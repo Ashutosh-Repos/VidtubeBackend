eb7162ff24ca854e6e669af8b981ba82c5955cfd
Ashutosh Kumar <ashutoshkumar@Ashutoshs-MacBook-Air-2.local>
2025-02-22 16:14:18 +0530

    getPublic given no auth acces

--- src/app.js
+++ src/app.js
@@ -1,45 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
-import {ApiError} from "./utils/ApiError.js"
-import  {auth}  from './middlewares/auth.middlewares.js';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ApiError } from "./utils/ApiError.js";
+import { auth } from "./middlewares/auth.middlewares.js";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
-app.use(express.urlencoded({extended:true}))
+app.use(
+  cors({
+    //origin: [`${process.env.CORS_ORIGIN}`],
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 //app.use(express.urlencoded({extended: true, limit: '100kb'}));
-app.use(express.json({limit: '100kb'}));
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "100kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 // app.use(auth);
 
 //import routes
-import  healthCheckRouter  from "./routes/healthcheck.routes.js";
-import userRouter from "./routes/user.routes.js"
-import videoRouter from "./routes/video.routes.js"
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
-import playlistRouter from "./routes/playlist.routes.js"
+import playlistRouter from "./routes/playlist.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/likes.routes.js";
 
-
-
 //routes
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/video",auth,videoRouter);
-app.use("/api/v1/subscription",auth,subscriptionRouter);
-app.use("/api/v1/playlist",auth,playlistRouter);
-app.use("/api/v1/tweet",auth,tweetRouter);
-app.use("/api/v1/comment",auth,commentRouter);
-app.use("/api/v1/like",auth,likeRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/subscription", auth, subscriptionRouter);
+app.use("/api/v1/playlist", auth, playlistRouter);
+app.use("/api/v1/tweet", auth, tweetRouter);
+app.use("/api/v1/comment", auth, commentRouter);
+app.use("/api/v1/like", auth, likeRouter);
 
-
-
-
-export {app}
+export { app };
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,33 +1,41 @@
 import { Router } from "express";
-import {publishAVideo, updateVideo, deleteVideo, getVideoById, getAllVideos, togglePublishStatus} from "../controllers/video.controllers.js"
+import {
+  publishAVideo,
+  updateVideo,
+  deleteVideo,
+  getVideoById,
+  getAllVideos,
+  togglePublishStatus,
+} from "../controllers/video.controllers.js";
 import { auth } from "../middlewares/auth.middlewares.js";
-import {upload} from "../middlewares/multer.middlewares.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 import { conversion } from "../middlewares/conversion.middlewares.js";
 const router = Router();
 
 router.route("/").get(getAllVideos);
 
 router.route("/upload").post(
-    auth,
-    upload.fields([
-        {
-            name: "video",
-            maxCount: 1
-        }, 
-        {
-            name: "thumbnail",
-            maxCount: 1
-        }
-    ]),
-    conversion,
-    publishAVideo
+  auth,
+  upload.fields([
+    {
+      name: "video",
+      maxCount: 1,
+    },
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+  ]),
+  conversion,
+  publishAVideo
 );
 
-router.route("/:videoId")
-    .get(getVideoById)
-    .delete(deleteVideo)
-    .patch(upload.single("thumbnail"),updateVideo);
+router
+  .route("/:videoId")
+  .get(auth, getVideoById)
+  .delete(auth, deleteVideo)
+  .patch(auth, upload.single("thumbnail"), updateVideo);
 
-router.route("/update/toggle/:videoId").patch(togglePublishStatus);
+router.route("/update/toggle/:videoId").patch(auth, togglePublishStatus);
 
 export default router;
